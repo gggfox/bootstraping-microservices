3c203bc456fd3bfd69e78e842853a94eec5cb41e
gggfox <gerardogalangarzafox@gmail.com>
2022-10-07 15:06:01 -0500

    update tf backend

--- scripts/backend.tf
+++ scripts/backend.tf
@@ -3,7 +3,7 @@
 
 terraform {
     backend "azurerm" {
-        resource_group_name  = "flixtube-terraform"
+        resource_group_name  = "filxtube-terraform"
         storage_account_name = "flixtubeterraform97"
         container_name       = "terraform-state"
         key                  = "terraform.tfstate"
